739024561cb0d44f8e9d5c3cb8b6135658741d53
OhhDann <dannyjones896@hotmail.co.uk>
2014-06-23 14:53:14 +0100

    Update and rename hello.py to Game1.py

---
+++ Game1.py
@@ -0,0 +1,10 @@
+playing = True
+answer = 30
+guesses = 5
+Print(''' 
+We will be playing a guessing game - you will guess a number between 1 and 100
+i will tell you if you are too high or too low, You will have 5 guesses in total
+''')
+
+
+
--- hello.py
+++
@@ -1,5 +0,0 @@
-def hello(name):
-    print ('hello {0}'.format(name))
-
-hello('Dan')
-hello ('mr Jones')
